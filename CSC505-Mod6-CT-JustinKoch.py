5d38883ce9cc7954a8c4582b6b273de84cdb9246
JustinCurtisKoch <justincurtiskoch@gmail.com>
2024-12-30 13:28:52 -0500

    Mod6-CT-v3

--- CSC505-Mod6-CT-JustinKoch.py
+++ CSC505-Mod6-CT-JustinKoch.py
@@ -8,34 +8,34 @@ class Job:
         self.processing_time = processing_time
         self.process_id = process_id
 
-# Create the Shortest Job First function
-def shortest_job_first(jobs):
-    time = 0                    # initial system time
-    chart_metrics = []          # will be used to track how jobs complete
-    completed_jobs = {}         # creates a store for completed jobs
+    # Create the Shortest Job First function
+    def shortest_job_first(jobs):
+        time = 0                    # initial system time
+        chart_metrics = []          # will be used to track how jobs complete
+        completed_jobs = {}         # creates a store for completed jobs
 
-# Create the main loop that will iterate through received jobs
-    while jobs:
-        current_jobs = []
-        for job in jobs:
-            if job.arrival_time <= time:
-                current_jobs.append(job)
+    # Create the main loop that will iterate through received jobs
+        while jobs:
+            current_jobs = []
+            for job in jobs:
+                if job.arrival_time <= time:
+                    current_jobs.append(job)
 
-        if current_jobs == []:
-            time += 1
-            chart_metrics.append("Idle")
-        else:
-            current_jobs.sort(key=lambda x: x.processing_time) # Sort current jobs by processing time
-            job = current_jobs[0]
-            time += job.processing_time
-            chart_metrics.append(job.process_id)
-            current_time = time                                # Set time and compute results based on run times
-            turnaround_time = current_time - job.arrival_time
-            wait_time = turnaround_time - job.processing_time
-            jobs.remove(job)                                   # Remove job from list once complete
-            completed_jobs[job.process_id] = [current_time, turnaround_time, wait_time]
+            if current_jobs == []:
+                time += 1
+                chart_metrics.append("Idle")
+            else:
+                current_jobs.sort(key=lambda x: x.processing_time) # Sort current jobs by processing time
+                job = current_jobs[0]
+                time += job.processing_time
+                chart_metrics.append(job.process_id)
+                current_time = time                                # Set time and compute results based on run times
+                turnaround_time = current_time - job.arrival_time
+                wait_time = turnaround_time - job.processing_time
+                jobs.remove(job)                                   # Remove job from list once complete
+                completed_jobs[job.process_id] = [current_time, turnaround_time, wait_time]
 
-    return chart_metrics, completed_jobs  # Return the results
+        return chart_metrics, completed_jobs  # Return the results
 
 # Create a sample list of jobs
 if __name__ == "__main__":
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         Job(3, 5, "P5"),
         Job(3, 3, "P6"),
     ]
-    chart_metrics, completed_jobs = shortest_job_first(jobs)  # Call the function and print results
+    chart_metrics, completed_jobs = Job.shortest_job_first(jobs)  # Call the function and print results
     print(f"Completion Order: {chart_metrics}")
     print("Completed Jobs:")
     for process_id, values in completed_jobs.items():
